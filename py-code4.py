8342fd6e573395c4408af188ecb29381454c247b
AtlasOfArthur <arthur.harjama@gmail.com>
2023-10-07 16:27:17 +0300

    Pikku muutoksia ja lisää kommentteja

--- py-code4.py
+++ py-code4.py
@@ -17,20 +17,21 @@
 animal = ["Elephant", "Cow", "Dog", "Rat"]
 item = ["Galaxy", "Solarsystem", "Moon", "Car", ]
 
-print("The {2} jumped over the {1}, and {0} sneezed. {2} ...was stunned!".format(animal[3],item[0],animal[2])) # OOO.. se toimiii! ^^
-
+print("The {2} jumped over the {1}, and {0} sneezed. {2:30} ...was stunned!".format(animal[3],item[0],animal[2])) # OOO.. se toimiii! ^^
+                   # animal[2] ja padding 10 yksikköä ^ ^^
  # Parempi tapa:
 text = ("The {} jumped over the {}, and {} sneezed. {:10} ...was stunned!")
                           # Add (10) padding to right ^
+    # {:<10} padding vasemmalla, {:>10} Padding oikealla, {:^10} Padding keskitettynä
 print(text.format(animal[1],item[1],animal[2],animal[3]))
 print()
 padding = 10 # voit määrittää paddingin sijainnin käyttämällä .rjust(), .ljust(), tai .center() 
 text2 = ("The {} jumped over the {}, and {} sneezed. {} ...was stunned!")
-
+ # Paddingin määrittely print lausekkeessa
 print(text2.format(animal[3], item[0], animal[0], animal[1].center(padding))+" (Keskitetty)")
 print(text2.format(animal[3], item[0], animal[0], animal[1].ljust(padding))+" (Padding oikealla)")
 print(text2.format(animal[3], item[0], animal[0], animal[1].rjust(padding))+" (Padding vasemmalla)")
-
+  # Siinä joitakin tapoja käyttää .format metodia ja paddingeja
 print("------------------------------------------------------------------------")
 print()
 #------------------------------------------------------------------------------#
